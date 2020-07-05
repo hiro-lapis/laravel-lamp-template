a47e590099cd16ac16bbff704975702067211bea
hiro-lapis <coolish555ddddd@gmail.com>
2020-07-05 20:39:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# laravel-lamp-template
+
+### 準備① docker-compose.ymlのDB項目(environment)をアプリに合わせて編集、このシェルと同じ階層に置いておくこと
+### 準備② このシェルのmy-siteの部分をアプリ名にリネーム
+### 準備③ docker-compose.ymlのmysqlのバージョンを任意に編集
+### 準備④ DockerfileのPHPバージョンを任意に編集
---
+++ startup.sh
@@ -0,0 +1,29 @@
+# 参考にしたHP https://mossa.dev/post/laravel_docker_lamp/
+
+# 事前準備① docker-compose.ymlのDB項目(environment)をアプリに合わせて編集、このシェルと同じ階層に置いておくこと
+# 事前準備② このシェルのmy-siteの部分をアプリ名にリネーム
+# 事前準備③ docker-compose.ymlのmysqlのバージョンを任意に編集
+# 事前準備④ DockerfileのPHPバージョンを任意に編集
+
+# 準備が終わったら、「sh startup.sh」で起動
+docker run --rm -v $(pwd):/app composer create-project --prefer-dist laravel/laravel my-site
+
+# プロジェクト内部に入り、appと同階層にdocker-compose.ymlを配置
+mv docker-compose.yml ./my-site
+# ymlと組み合わせて使うファイルを移動する
+# webコンテナDockerfile
+# apche設定ファイル
+# php設定ファイル
+mv docker ./my-site
+
+# コンテナ群の起動
+cd my-site
+docker-compose up -d
+
+# 最後に、docker-compose.ymlに合わせたDB設定をlaravelに書いて終了!
+# DB_CONNECTION=mysql(コンテナ)
+# DB_HOST=mysql
+# DB_PORT=3306
+# DB_DATABASE=db
+# DB_USERNAME=user
+# DB_PASSWORD=pw
