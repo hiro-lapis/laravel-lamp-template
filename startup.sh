08b9ebd8beb9fc1ec3174c76dedc80120a2b15ec
hiro-lapis <coolish555ddddd@gmail.com>
2021-12-31 00:02:20 +0900

    shellの更新

--- startup.sh
+++ startup.sh
@@ -6,33 +6,31 @@
 # 事前準備④ DockerfileのPHPバージョンを任意に編集
 
 # 準備が終わったら、「sh startup.sh」で起動
-# バージョン設定も忘れずに
-docker run --rm -v $(pwd):/app composer create-project  laravel/laravel my-site --prefer-dist "6.0.*"
+docker run --rm -v $(pwd):/app composer create-project  laravel/laravel new-haikishare --prefer-dist "^8.0"
 
 # プロジェクト内部に入り、appと同階層にdocker-compose.ymlを配置
-mv docker-compose.yml ./my-site
+mkdir new-haikishare
+mv docker-compose.yml ./new-haikishare
 # ymlと組み合わせて使うファイルを移動する
 # webコンテナDockerfile
 # apche設定ファイル
 # php設定ファイル
-mv docker ./my-site
+mv docker ./new-haikishare
 
-# コンテナ群の起動 キャッシュを使わずに、デタッチで起動する
-# no-cacheしないと過去の設定が生きたコンテナが立ってしまうことがある
-# 既に他のプロジェクトで同様のコンテナを立てている際は、--force-recreate で強制的にコンテナを作り直すことができる
-cd my-site
-docker-compose up -d --no-cache
+# コンテナ群の起動
+cd new-haikishare
+docker-compose up -d
 
-docker exec -it my-site_web_1 bash
-chmod -R 777 /var/www/myapp/storage
-chmod -R 777 /var/www/myapp/bootstrap
+docker exec -it new-haikishare_web_1 bash
+chmod -R 777 /var/www/new-haikishare/storage
+chmod -R 777 /var/www/new-haikishare/bootstrap
 php artisan key:generate
 
 # docker-compose.ymlに合わせたDB設定をlaravelの.envにに記述
 # 最後に、php artisan migrateして終了!
 
 # DB_CONNECTION=mysql
-# DB_HOST=mysql(docker ps コマンドで 表示されるservicesに書いてあるDBコンテナ名)
+# DB_HOST=mysql(servicesに書いてあるDBサービス名)
 # DB_PORT=3306
 # DB_DATABASE=db
 # DB_USERNAME=user
